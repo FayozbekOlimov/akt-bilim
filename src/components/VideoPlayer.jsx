026944a2a0f7ffe98828cbd274189c088ff8946a
FayozbekOlimov <fayozbek2021dev@gmail.com>
2023-04-15 23:30:34 +0500

    video resourses page

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,48 +1,129 @@
-import React, { useState } from "react";
-import ReactPlayer from "react-player";
-import { Container } from "@mui/material";
+import { useState } from "react";
 import { styled } from "@mui/material/styles";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  IconButton,
+  Grid,
+} from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import ReactPlayer from "react-player";
 
-const PlayerWrapper = styled("div")({
-  position: "relative",
-  paddingTop: "56.25%" /* 16:9 ratio */,
-});
+const VideoCard = styled(Card)(({ theme }) => ({
+  maxWidth: "100%",
+  maxHeight: "50vh",
+  [theme.breakpoints.down("xs")]: {
+    maxHeight: "30vh",
+  },
+}));
 
-// const StyledReactPlayer = styled(ReactPlayer)({
-//   position: "absolute",
-//   top: 0,
-//   left: 0,
-// });
+const Title = styled(Typography)(({ theme }) => ({
+  fontSize: "2rem",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1.5rem",
+  },
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "1rem",
+  },
+}));
 
-const VideoPlayer = ({ url }) => {
+const Description = styled(Typography)(({ theme }) => ({
+  fontSize: "1.2rem",
+  marginBottom: "1rem",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1rem",
+  },
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "0.8rem",
+    marginBottom: "0.5rem",
+  },
+}));
+
+const PlayPauseButton = styled(IconButton)(({ theme }) => ({
+  [theme.breakpoints.down("xs")]: {
+    display: "none",
+  },
+}));
+
+function VideoPlayer({ videoUrl }) {
+  const [playing, setPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    setPlaying(!playing);
+  };
+
   return (
-    <Container maxWidth="md">
-      <PlayerWrapper>
-        <ReactPlayer
-          url={url}
-          width="100%"
-          height="100%"
-          controls={true}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-          }}
-          youtubeConig={{
-            playerVars: {
-              autoplay: 0,
-              controls: 0,
-              disablekb: 1,
-              enablejsapi: 1,
-              modestbranding: 1,
-              showinfo: 0,
-            },
-          }}
-        />
-        <div></div>
-      </PlayerWrapper>
-    </Container>
+    <Grid item xs={12} sm={6} md={4}>
+      <VideoCard>
+        <CardMedia sx={{ width: "300px", height: "400px" }}>
+          <ReactPlayer url={videoUrl} playing={playing} />
+        </CardMedia>
+        {/* <CardContent>
+          <Title gutterBottom variant="h5" component="h2">
+            Video Title
+          </Title>
+          <Description variant="body2" color="textSecondary" component="p">
+            Video description
+          </Description>
+          <PlayPauseButton onClick={handlePlayPause}>
+            {playing ? <PauseIcon /> : <PlayArrowIcon />}
+          </PlayPauseButton>
+        </CardContent> */}
+      </VideoCard>
+    </Grid>
   );
-};
+}
 
 export default VideoPlayer;
+
+// import React from "react";
+// import ReactPlayer from "react-player";
+// import { Container } from "@mui/material";
+// import { styled } from "@mui/material/styles";
+
+// const PlayerWrapper = styled("div")({
+//   position: "relative",
+//   paddingTop: "56.25%" /* 16:9 ratio */,
+// });
+
+// // const StyledReactPlayer = styled(ReactPlayer)({
+// //   position: "absolute",
+// //   top: 0,
+// //   left: 0,
+// // });
+
+// const VideoPlayer = ({ url }) => {
+//   return (
+//     <Container maxWidth="md">
+//       <PlayerWrapper>
+//         <ReactPlayer
+//           url={url}
+//           width="100%"
+//           height="100%"
+//           controls={true}
+//           style={{
+//             position: "absolute",
+//             top: 0,
+//             left: 0,
+//           }}
+//           youtubeConfig={{
+//             playerVars: {
+//               autoplay: 0,
+//               controls: 0,
+//               disablekb: 1,
+//               enablejsapi: 1,
+//               modestbranding: 1,
+//               showinfo: 0,
+//             },
+//           }}
+//         />
+//         <div></div>
+//       </PlayerWrapper>
+//     </Container>
+//   );
+// };
+
+// export default VideoPlayer;
--- src/pages/VideoResources/index.jsx
+++ src/pages/VideoResources/index.jsx
@@ -1,17 +1,91 @@
-import { Box, Grid } from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Chip,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
-import VideoPlayer from "../../components/VideoPlayer";
+import { Link } from "react-router-dom";
+import PlayArrowIcon from "@mui/icons-material/PlayArrowRounded";
+import { keyframes } from "@emotion/react";
+import { DateRange } from "@mui/icons-material";
 
+const playAnimation = keyframes`
+  0% {
+    transform: translateX(-50%) scale(1);
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0.3);
+  }
+  50% {
+    transform: translateX(-50%) scale(1.05);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.7);
+  }
+  100% {
+    transform: translateX(-50%) scale(1);
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0.3);
+  }
+`;
 const VideoResources = () => {
   return (
-    <Grid
-      container
-      rowGap={3}
-    >
+    <Grid container spacing={2}>
       {Array.from(Array(6)).map((_, index) => (
-        <Grid xs={12} md={6} lg={4} key={index}>
-          <VideoPlayer url={"https://youtu.be/ewZ_YWbIWXI"} />
+        <Grid item xs={12} md={6} lg={4} key={index}>
+          <Card
+            component={Link}
+            to="/"
+            sx={{
+              textDecoration: "none",
+              display: "block",
+              borderRadius: 1,
+            }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="180"
+                image="/images/subject.jpg"
+                alt="green iguana"
+              />
+              <Stack sx={{ position: "absolute", top: "4px", right: "4px" }}>
+                <Chip
+                  size="small"
+                  label={
+                    <Stack direction="row" alignItems={"center"} gap={0.5}>
+                      <Typography variant="body2" component={"span"}>
+                        04-15-2023
+                      </Typography>
+                    </Stack>
+                  }
+                  color="secondary"
+                />
+              </Stack>
+              <PlayArrowIcon
+                sx={{
+                  position: "absolute",
+                  left: "50%",
+                  top: "60px",
+                  transform: "translateX(-50%)",
+                  width: "60px",
+                  height: "60px",
+                  color: "secondary.main",
+                  borderRadius: "50%",
+                  boxShadow: "0 0 0 rgba(0, 0, 0, 0.3)",
+                  animation: `${playAnimation} 1.2s ease-in-out infinite`,
+                }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Matematika
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Natural sonlaring bo'linish alomatlari
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Grid>
       ))}
     </Grid>
